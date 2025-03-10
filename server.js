ba71ee0161a4262baf92e30997eabef89f10fbc3
Carlitzo <adrianscholl95@hotmail.com>
2025-03-10 11:05:53 +0100

    updated server

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-import { serveTls } from "https://deno.land/std@0.120.0/http/server.ts";
-import { serveFile } from "https://deno.land/std@0.120.0/http/file_server.ts";
-import { serveDir } from "jsr:@std/http";
+import { serveDir } from "https://deno.land/std@0.120.0/http/file_server.ts";
 
+// För loggning med stacktrace
 const originalLog = console.log;
-
 console.log = (...args) => {
   const stack = new Error().stack?.split("\n")[2] || "Unknown location";
   const location = stack.trim().replace(/^at\s+/, "");
   originalLog(`[${location}]`, ...args);
 };
 
-const certPath =  await Deno.readTextFile("./certs/cert.pem");
-const keyPath =  await Deno.readTextFile("./certs/private.pem");
+// Starta en server som serverar filer från public/
+await Deno.serve(
+  {
+    hostname: "0.0.0.0",  // Också användbart för Deno Deploy
+    port: 8888            // Deno Deploy kommer att hantera HTTPS åt dig
+  },
+  (req) => {
+    // Servera filer från public/
+    return serveDir(req, { fsRoot: "./public/", quiet: true });
+  }
+);
 
-
-Deno.serve({
-  hostname: "0.0.0.0",
-  port: 8888,
-  cert: certPath,
-  key: keyPath },
-(req) => {
-  return serveDir(req, {fsRoot: "./public/", quiet: true})
-});
+console.log("Server running");
